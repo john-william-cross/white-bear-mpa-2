6c373dd0710f288a060517b2ab0bda4c2e28bb9c
John Cross <john.william.cross@gmail.com>
2021-01-05 10:03:44 -0800

    install react-router-dom, add whitebear routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
-import "./style/master.scss";
+import "./style/master.scss"; // applies global scss styles
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-danger">Hello World</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/create-answer" component={CreateAnswer} />
+        <Route exact path="/create-imagery" component={CreateImagery} />
+        <Route exact path="/review-imagery" component={ReviewImagery} />
+        <Route exact path="/review-answer" component={ReviewAnswer} />
+        <Route exact path="/review-empty" component={ReviewEmpty} />
+        <Route exact path="/all-cards" component={AllCards} />
+        <Route exact path="/edit" component={Edit} />
+        <Route component={NotFound} />{" "}
+        {/* if someone types a url that doesn't exist, this will display the not found page*/}
+      </Switch>
+    </Router>
   );
 }
 
